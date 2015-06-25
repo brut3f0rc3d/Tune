3f1d70c5c0108f042232517291c015d7e8ae3738
brut3f0rc3d <palash.s.chatterjee@gmail.com>
2015-06-25 21:30:46 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tune - Your Premium Music Destination</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<nav class="navbar">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">
+					<img src="images/logo.png" height="50px" width="50px" style="display: inline">
+					<h1 style="font-family: 'Montserrat', sans-serif; display:inline; color: #23527c">tune <small>BETA</small></h1>
+				</a>
+			</div>
+
+			<div class="navbar-collapse collapse">
+				<form class="navbar-form navbar-right tune-form">
+					<div class="form-group">
+						<input type="text" placeholder="Email Address (johndoe@example.com)" class="form-control">
+					</div>
+					<button type="submit" class="btn btn-default">Play</button>
+				</form>
+				<ul class="nav navbar-nav navbar-right navbar-ul">
+					<li><a href="about">ABOUT US</a></li>
+					<li><a href="pricing">PRICING</a></li>
+					<li><a href="contact">CONTACT</a></li>
+				</ul>
+				
+			</div>
+		</div>
+			
+	</nav>
+	<div class="landing slide">
+		<div class="landing-img">
+			<img src="images/3.jpg" height="500px" width="700px">
+		</div>
+		<button type="button" class="btn btn-default round-button" style="position: absolute; bottom: 20px; left: 48%">
+			<i class="fa fa-lg fa-angle-down"></i>
+		</button>
+	</div>
+	<div class="about slide">
+		<div class="container">
+			<div class="slide-header">ABOUT US</div>
+			<div class="slide-body">Lorem Ipsum</div>
+		</div>
+	</div>
+	<div class="pricing slide"></div>
+	<div class="contact slide"></div>
+</body>
+</html>
